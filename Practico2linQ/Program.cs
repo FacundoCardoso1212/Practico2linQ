9ac502820022b587715f2c5394e8db410e1d00a8
facundo <facundocardoso121201@gmail.com>
2024-04-19 21:32:43 -0300

    cambios, me emocioné

--- Practico2linQ/Program.cs
+++ Practico2linQ/Program.cs
@@ -24,21 +24,27 @@ namespace Practico2linQ
 
         };
 
-
-            personas.OrderBy(p => p.Nombre).ToList() .Where(p => p.Edad > 30 && p.Ciudad == "Lima").ToList().ForEach(p => Console.WriteLine(p.Nombre));
+            Console.WriteLine("Mostrar el nombre de todas las personas que tengan una edad mayor a 30 y vivan en Bogotá.");
+            personas.OrderBy(p => p.Nombre).ToList() .Where(p => p.Edad > 30 && p.Ciudad == "Bogota").ToList().ForEach(p => Console.WriteLine(p.Nombre));
             Console.ReadLine();
 
-            //Ejercicio 2
-
-             personas.Where(p => p.Edad >= 25 && p.Edad <= 35).ToList().OrderBy(p => p.Nombre).ToList().ForEach(p => Console.WriteLine(p.Nombre));
+            Console.WriteLine("Mostrar el nombre de todas las personas que tengan una edad entre 25 y 35 años, ordenadas por\r\nedad de forma ascendente.");
+            personas.Where(p => p.Edad >= 25 && p.Edad <= 35).ToList().OrderBy(p => p.Nombre).ToList().ForEach(p => Console.WriteLine(p.Nombre));
              Console.ReadLine();
+            
+            //Ejercicio 2
 
             ControlEmpresasEmpleados prueba1 = new ControlEmpresasEmpleados();
 
+            Console.WriteLine("Empleados con cargo CEO. ");
+
             prueba1.getCeo("CEO");
+
+            Console.WriteLine("Lista empleados:  ");
 
             prueba1.getEmpleadosOrdenados();
 
+            //Ejercicio 3b:
 
             List<int> valores = new List<int>() { 1, 32, 22, 13, 98, 8, 7, 4, 16 };
 
@@ -54,15 +60,16 @@ namespace Practico2linQ
                 }
             }
 
+            Console.Write("Ejercicio 3b: ");
             foreach (var valorOrdenado in valores)
             {
                 Console.WriteLine(valorOrdenado);
             }
 
-            //Ejecicio 3b:
             valores.OrderByDescending(x => x).ToList().ForEach(x => Console.WriteLine(x));
 
             //Ejercicio 4 a/b:
+            Console.Write("Ejercicio 4 a/b:");
 
             Console.WriteLine(valores.Sum());
 
@@ -72,6 +79,7 @@ namespace Practico2linQ
             //Ejercicio 5.1 = 7.
 
             //Eercicio 5.2
+            Console.Write("Eercicio 5.2");
 
             List<int> valores2 = new List<int>() { 1, 5, 6, 32, 22, 13, 98, 8, 7, 4, 16 };
 
@@ -85,6 +93,7 @@ namespace Practico2linQ
             //Ejercicio 6.1 = 6.
             //Ejercicio 6.2 = 4
 
+            Console.Write("Ejercicio 6: ");
 
             Console.WriteLine("Coloque una letra de la A a la F");
             string letra = Console.ReadLine();
@@ -101,6 +110,7 @@ namespace Practico2linQ
                 else if (letras[i] == letra)
                 {
                     Console.WriteLine($"La letra siguiente del abecedario es {letras[i + 1]}");
+                    Console.ReadKey();
                     break;
                 }
             }
